b70633de713141bd43111c860b7cd54c22a6195b
Simon Dobson <simon.dobson@computer.org>
2021-11-26 16:27:29 +0000

    Added network access

--- epydemic_signals/signalgenerator.py
+++ epydemic_signals/signalgenerator.py
@@ -18,6 +18,7 @@
 # along with epydemic-signals. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 from typing import Callable
+from networkx import Graph
 from epydemic import Element, Node, Edge
 from epydemic_signals import Signal
 
@@ -29,10 +30,13 @@ EventHandler =  Callable[[float, Element], None]   #: Type of event-handler func
 class SignalGenerator:
     '''The base class for signal generators. A signal generator is
     attached to a signal dynamics to repond to the events generated
-    by a simulation, generating the corresponding signal.'''
+    by a simulation, generating the corresponding signal.
 
-    def __init__(self, s: Signal = None):
+    :param s: the signal being generated'''
+
+    def __init__(self, s: Signal):
         self._signal = s
+        self._network = s.network()
         self._typeHandler: Dict[str, EventHandler] = dict()
 
     def signal(self) -> Signal:
@@ -40,6 +44,12 @@ class SignalGenerator:
 
         :returns: the signal'''
         return self._signal
+
+    def network(self) -> Graph:
+        '''Return the network the signal is being genearted for.
+
+        :returns: the network'''
+        return self._network
 
     def addEventTypeHandler(self, etype: str, eh: EventHandler):
         '''Register a handler for the given event. The handler is called
@@ -64,6 +74,7 @@ class SignalGenerator:
         :param t: the simulation time
         :param etype: the event type
         :param e: the element'''
+        print(t, etype, e)
         ehs = self._typeHandler.get(etype, [])
         for eh in ehs:
             eh(t, e)
