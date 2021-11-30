67d37e153f22eed4b68ff731ba60a2f04998c74a
Simon Dobson <simon.dobson@computer.org>
2021-11-30 09:16:03 +0000

    Added iinit file for tests package

---
+++ test/__init__.py
@@ -0,0 +1,18 @@
+# Initialistion for the epydemic_signals tests
+#
+# Copyright (C) 2021 Simon Dobson
+#
+# This file is part of epydemic-signals, an experiment in epidemics processes.
+#
+# epydemic-signals is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# epydemic-signals is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with epydemic-signals. If not, see <http://www.gnu.org/licenses/gpl.html>.
